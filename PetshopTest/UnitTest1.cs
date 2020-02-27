553b9da4cd6600fc9f4781c1b410084dbe78895a
wees-guimaraes <wees.guimaraes@gmail.com>
2020-02-26 21:01:56 -0300

    Adição da pasta de teste

---
+++ PetshopTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Petshop;
+namespace PetshopTest
+{
+    [TestClass]
+    public class TesteUnitario{
+        Controller control = new Controller();
+
+
+        [TestMethod]
+        public void createTest(){
+          var resultado = control.registrarServico("Teste Serviço", 100);
+            Assert.AreEqual(true, resultado);
+        }
+        [TestMethod]
+        public void listTest(){
+            var resultado = control.exibirServicos();
+            Assert.AreEqual(true, resultado);
+        }
+
+      /*  [TestMethod]
+        public void updateTest(){
+
+        }*/
+
+       /* [TestMethod]
+        public void deleteTest(){
+
+        }*/
+
+    }
+}
