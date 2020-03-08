aa32200be894ed4477801ac08c9c774ee76e3176
wees-guimaraes <wees.guimaraes@gmail.com>
2020-03-07 21:28:09 -0300

    ADICAÇÃO DO TESTE

---
+++ PetshopTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PetshopTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PetshopTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e779f10d-cd5c-4b2e-89da-c80c354a0638")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PetshopTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Petshop;
+namespace PetshopTest
+{
+    [TestClass]
+    public class TesteUnitario{
+        Controller control = new Controller();
+        int ultimoId;
+
+        [TestInitialize]
+        public void prepararTests() {
+            control.limpaBanco();
+        }
+
+        [TestMethod]
+        public void createTest(){
+          var resultadoInsert = control.registrarServico("Serviço Inserido", 200.0);
+            Assert.AreEqual(true, resultadoInsert);
+        }
+
+        [TestMethod]
+        public void listTest(){
+            var resultado = control.exibirServicos();
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+        public void updateTest(){
+            control.registrarServico("Teste", 100.0);
+            ultimoId = control.retornaUltimoId();
+            var resultado = control.editarServico("Teste", "Teste Editado", 50.0, ultimoId);
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+        public void deleteTest(){
+            control.registrarServico("Teste a Deletar", 90.0);
+            ultimoId = control.retornaUltimoId();
+            var resultadoDelete = control.deletarServico(ultimoId);
+            Assert.AreEqual(true, resultadoDelete);
+        }
+
+    }
+}
